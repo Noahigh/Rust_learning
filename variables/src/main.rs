243ede96fa7ca621e7113eca1eacac2eeef6e6e3
Noah <noghspace@hotmail.com>
2024-01-05 23:00:35 +0800

    Update main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,10 +2,20 @@ fn main() {
     println!("Hello, world!");
 
     let x = 5;
-    println!("The x is: {}", x);
+
+    println!("The value of x is: {}", x);
 
     // x = 6;  // 这是错误的
-
     let x = 6;
-    println!("The x is: {}", x);
+    println!("The value of x is: {}", x);
+
+    // ==========================
+    let mut y = 2;  // 声明可变变量
+
+    println!("The value of y is: {}", y);
+
+    y = 3;
+
+    println!("The value of y is: {}", y);
+
 }
