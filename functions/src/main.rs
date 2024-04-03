ba2bd360de924288eb6c7893ca9d5d7b8cb037ad
Noah <noghspace@hotmail.com>
2024-04-03 17:32:53 +0800

    Dairy Update.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,11 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 7);
+
+    let mut x = 2;
+    x = another_function_1(x, 7);
+
+    println!("The value of x is: {}.", x);
 }
 
 
@@ -9,4 +14,9 @@ fn another_function(x: i32, y: i32) {
     println!("Another function.");  // 这是一个语句
     println!("The value of x is: {}.", x);  // 这是一个语句
     println!("The value of y is: {}.", y);  // 这是一个语句
+}
+
+
+fn another_function_1(mut x: i32, y: i32) -> i32{
+    x + y
 }
